6da89685b79d272fa08e9a847367cf74b10e5f88
Siarhei Dziki <siarhei.dziki@gmail.com>
2020-07-23 20:24:51 +0300

    - CMake unix fix and improvement

--- src/render/vulkan/vk_renderer.cxx
+++ src/render/vulkan/vk_renderer.cxx
@@ -449,7 +449,7 @@ void vk_renderer::createGraphicsPipeline()
 	rasterizationState.rasterizerDiscardEnable = VK_FALSE;
 	rasterizationState.polygonMode = VK_POLYGON_MODE_FILL;
 	rasterizationState.lineWidth = 1.0f;
-	rasterizationState.cullMode = VK_CULL_MODE_BACK_BIT;
+	rasterizationState.cullMode = VK_CULL_MODE_NONE;
 	rasterizationState.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 	rasterizationState.depthBiasEnable = VK_FALSE;
 	rasterizationState.depthBiasConstantFactor = 0.0f;
