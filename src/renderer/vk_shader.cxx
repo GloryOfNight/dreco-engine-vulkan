fe093f7a5157605e708779b2c1cb2754b9fe19c7
Sergey Dikiy <siarhei.dziki@gmail.com>
2022-07-05 23:57:33 +0300

    - added some shader reflection to shaders

--- src/renderer/vk_shader.cxx
+++ src/renderer/vk_shader.cxx
@@ -5,6 +5,8 @@
 
 #include "dreco.hxx"
 
+#include <spirv-reflect/spirv_reflect.h>
+
 vk_shader::~vk_shader()
 {
 	destroy();
@@ -12,7 +14,13 @@ vk_shader::~vk_shader()
 
 void vk_shader::create()
 {
-	_shaderModule = loadShader(_shaderPath, vk_renderer::get()->getDevice());
+	std::string shaderCode;
+	file_utils::readFile(_shaderPath, shaderCode);
+	if (!shaderCode.empty())
+	{
+		_shaderModule = vk_renderer::get()->getDevice().createShaderModule(vk::ShaderModuleCreateInfo({}, shaderCode.size(), reinterpret_cast<uint32_t*>(shaderCode.data())));
+		descriptShader(shaderCode);
+	}
 }
 
 void vk_shader::destroy()
@@ -22,7 +30,6 @@ void vk_shader::destroy()
 		vk_renderer::get()->getDevice().destroyShaderModule(_shaderModule);
 		_shaderModule = nullptr;
 	}
-	
 }
 
 bool vk_shader::isValid() const
@@ -35,13 +42,81 @@ std::string_view vk_shader::getPath() const
 	return _shaderPath;
 }
 
-vk::ShaderModule vk_shader::loadShader(const std::string_view& path, const vk::Device device)
+const vk::PipelineShaderStageCreateInfo& vk_shader::getPipelineShaderStageCreateInfo() const
 {
-	std::string shaderCode;
-	file_utils::readFile(path, shaderCode);
-	if (!shaderCode.empty())
+	return _pipelineShaderStageCreateInfo;
+}
+
+const std::vector<vk_descriptor_shader_data> vk_shader::getDescirptorShaderData() const
+{
+	return _descirptorShaderData;
+}
+
+const std::vector<vk::PushConstantRange> vk_shader::getPushConstantRanges() const
+{
+	return _pushConstantRanges;
+}
+
+void vk_shader::descriptShader(const std::string_view& shaderCode)
+{
+	SpvReflectShaderModule reflModule;
+	const SpvReflectResult result =
+		spvReflectCreateShaderModule(shaderCode.size(), reinterpret_cast<const uint32_t*>(shaderCode.data()), &reflModule);
+
+	// spirv failed to reflect, investigate required
+	assert(result == SPV_REFLECT_RESULT_SUCCESS);
+
+	_pipelineShaderStageCreateInfo = vk::PipelineShaderStageCreateInfo()
+										 .setModule(_shaderModule)
+										 .setStage(static_cast<vk::ShaderStageFlagBits>(reflModule.shader_stage))
+										 .setPName(reflModule.entry_point_name);
+
+	_descirptorShaderData.resize(reflModule.descriptor_set_count, vk_descriptor_shader_data());
+	for (uint8_t i = 0; i < reflModule.descriptor_set_count; i++)
 	{
-		return device.createShaderModule(vk::ShaderModuleCreateInfo({}, shaderCode.size(), reinterpret_cast<uint32_t*>(shaderCode.data())));
+		const auto& reflDescSet = reflModule.descriptor_sets[i];
+		auto& descSetData = _descirptorShaderData[i];
+
+		descSetData._descriptorSetIndex = reflDescSet.set;
+		descSetData._descriptorSetLayoutBindings.resize(reflDescSet.binding_count, vk::DescriptorSetLayoutBinding());
+		for (uint8_t k = 0; k < reflDescSet.binding_count; k++)
+		{
+			const auto& reflBinding = reflModule.descriptor_bindings[i];
+			auto& binding = descSetData._descriptorSetLayoutBindings[i];
+
+			binding = vk::DescriptorSetLayoutBinding()
+						  .setBinding(reflBinding.binding)
+						  .setDescriptorType(static_cast<vk::DescriptorType>(reflBinding.descriptor_type))
+						  .setDescriptorCount(reflBinding.count)
+						  .setStageFlags(static_cast<vk::ShaderStageFlagBits>(reflModule.shader_stage));
+		}
+		descSetData._descriptorSetLayoutCreateInfo.setBindings(descSetData._descriptorSetLayoutBindings);
 	}
-	return nullptr;
+
+	_pushConstantRanges.resize(reflModule.push_constant_block_count, vk::PushConstantRange());
+	for (uint8_t i = 0; i < reflModule.push_constant_block_count; i++)
+	{
+		const auto& reflPushConstantRange = reflModule.push_constant_blocks[i];
+		auto& pushConstantRange = _pushConstantRanges[i];
+
+		pushConstantRange = vk::PushConstantRange()
+								  .setStageFlags(static_cast<vk::ShaderStageFlagBits>(reflModule.shader_stage))
+								  .setOffset(reflPushConstantRange.offset)
+								  .setSize(reflPushConstantRange.size);
+	}
+
+	spvReflectDestroyShaderModule(&reflModule);
+}
+
+std::vector<vk::DescriptorPoolSize> vk_descriptor_shader_data::getDescriptorPoolSizes() const
+{
+	std::vector<vk::DescriptorPoolSize> poolSizes(_descriptorSetLayoutBindings.size(), vk::DescriptorPoolSize());
+	for (uint8_t i = 0; i < poolSizes.size(); i++)
+	{
+		const auto& binding = _descriptorSetLayoutBindings[i];
+		poolSizes[i] = vk::DescriptorPoolSize()
+						   .setType(binding.descriptorType)
+						   .setDescriptorCount(binding.descriptorCount);
+	}
+	return poolSizes;
 }
--- src/renderer/vk_shader.hxx
+++ src/renderer/vk_shader.hxx
@@ -12,6 +12,17 @@ struct vk_descriptor_write_infos
 	std::vector<vk::DescriptorImageInfo> imageInfos;
 };
 
+struct vk_descriptor_shader_data
+{
+	uint32_t _descriptorSetIndex{UINT32_MAX};
+
+	vk::DescriptorSetLayoutCreateInfo _descriptorSetLayoutCreateInfo{};
+
+	std::vector<vk::DescriptorSetLayoutBinding> _descriptorSetLayoutBindings{};
+
+	std::vector<vk::DescriptorPoolSize> getDescriptorPoolSizes() const;
+};
+
 class vk_shader
 {
 public:
@@ -38,11 +49,22 @@ public:
 
 	virtual void cmdPushConstants(vk::CommandBuffer commandBuffer, vk::PipelineLayout pipelineLayout, const vk_mesh* mesh) const{};
 
+	const vk::PipelineShaderStageCreateInfo& getPipelineShaderStageCreateInfo() const;
+
+	const std::vector<vk_descriptor_shader_data> getDescirptorShaderData() const;
+	
+	const std::vector<vk::PushConstantRange> getPushConstantRanges() const;
+
 protected:
+	void descriptShader(const std::string_view& shaderCode);
+
 	std::string _shaderPath{};
 
 	vk::ShaderModule _shaderModule{};
 
-private:
-	vk::ShaderModule loadShader(const std::string_view& path, const vk::Device device);
+	vk::PipelineShaderStageCreateInfo _pipelineShaderStageCreateInfo{};
+
+	std::vector<vk_descriptor_shader_data> _descirptorShaderData;
+
+	std::vector<vk::PushConstantRange> _pushConstantRanges;
 };
