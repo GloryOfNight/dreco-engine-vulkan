ccecc8232ec7850fe2cd30be21219929d905de51
Sergey Dikiy <siarhei.dziki@gmail.com>
2022-10-19 18:57:37 +0300

    - fix for CIs

--- engine/src/core/utils/file_utils.hxx
+++ engine/src/core/utils/file_utils.hxx
@@ -3,33 +3,33 @@
 
 #include <filesystem>
 #include <fstream>
-#include <string>
+#include <string_view>
 
 class file_utils
 {
 public:
-	template<typename Str>
-	static std::string readFile(Str&& path)
+	static std::string readFile(const std::string_view path)
 	{
 		if (std::filesystem::is_regular_file(path))
 		{
-			std::ifstream file(path, std::ifstream::binary);
+			std::ifstream file(path.data(), std::ifstream::binary);
 			if (file.is_open())
 			{
 				const auto fileSize = std::filesystem::file_size(path);
 				std::string fileContent = std::string(fileSize, '\0');
 				file.read(fileContent.data(), fileContent.size());
 				file.close();
+				DE_LOG(Verbose, "%s: read %i bytes from: %s", __FUNCTION__, fileContent.size(), path.data());
 				return fileContent;
 			}
 			else
 			{
-				DE_LOG(Error, "%s: Failed to open file: %s", __FUNCTION__, path);
+				DE_LOG(Error, "%s: failed to open file: %s", __FUNCTION__, path.data());
 			}
 		}
 		else
 		{
-			DE_LOG(Error, "%s: Not a file: %s", __FUNCTION__, path);
+			DE_LOG(Error, "%s: not a file: %s", __FUNCTION__, path.data());
 		}
 		return std::string();
 	}
