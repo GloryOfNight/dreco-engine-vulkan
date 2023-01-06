45cc66d97e9938dda0ee35acf78ada8098f47ae7
Siarhei Dziki <siarhei.dziki@gmail.com>
2023-01-06 20:54:46 +0100

    - added namespaces for logging

--- engine/src/core/misc/log.hxx
+++ engine/src/core/misc/log.hxx
@@ -6,41 +6,45 @@
 
 namespace de
 {
-	enum class log_category
+	namespace log
 	{
-		Application = SDL_LOG_CATEGORY_APPLICATION,
-		Error = SDL_LOG_CATEGORY_ERROR,
-		Assert = SDL_LOG_CATEGORY_ASSERT,
-		System = SDL_LOG_CATEGORY_SYSTEM,
-		Audio = SDL_LOG_CATEGORY_AUDIO,
-		Video = SDL_LOG_CATEGORY_VIDEO,
-		Render = SDL_LOG_CATEGORY_RENDER,
-		Input = SDL_LOG_CATEGORY_INPUT,
-		Test = SDL_LOG_CATEGORY_TEST,
-	};
-	enum class log_priority
-	{
-		Verbose = SDL_LOG_PRIORITY_VERBOSE,
-		Debug = SDL_LOG_PRIORITY_DEBUG,
-		Info = SDL_LOG_PRIORITY_INFO,
-		Warn = SDL_LOG_PRIORITY_WARN,
-		Error = SDL_LOG_PRIORITY_ERROR,
-		Critical = SDL_LOG_PRIORITY_CRITICAL
-	};
 
-	template <typename... Args>
-	static void log(log_category category, log_priority priority, const std::string_view message, Args&&... args)
-	{
-		if constexpr (sizeof...(args) == 0)
+		enum class category
 		{
-			SDL_LogMessage(static_cast<SDL_LogCategory>(category), static_cast<SDL_LogPriority>(priority), "%s", message.data());
-		}
-		else
+			Application = SDL_LOG_CATEGORY_APPLICATION,
+			Error = SDL_LOG_CATEGORY_ERROR,
+			Assert = SDL_LOG_CATEGORY_ASSERT,
+			System = SDL_LOG_CATEGORY_SYSTEM,
+			Audio = SDL_LOG_CATEGORY_AUDIO,
+			Video = SDL_LOG_CATEGORY_VIDEO,
+			Render = SDL_LOG_CATEGORY_RENDER,
+			Input = SDL_LOG_CATEGORY_INPUT,
+			Test = SDL_LOG_CATEGORY_TEST,
+		};
+		enum class priority
 		{
-			SDL_LogMessage(static_cast<SDL_LogCategory>(category), static_cast<SDL_LogPriority>(priority), message.data(), std::forward<Args>(args)...);
+			Verbose = SDL_LOG_PRIORITY_VERBOSE,
+			Debug = SDL_LOG_PRIORITY_DEBUG,
+			Info = SDL_LOG_PRIORITY_INFO,
+			Warn = SDL_LOG_PRIORITY_WARN,
+			Error = SDL_LOG_PRIORITY_ERROR,
+			Critical = SDL_LOG_PRIORITY_CRITICAL
+		};
+
+		template <typename... Args>
+		static void message(category category_, priority priority_, const std::string_view format, Args&&... args)
+		{
+			if constexpr (sizeof...(args) == 0)
+			{
+				SDL_LogMessage(static_cast<SDL_LogCategory>(category_), static_cast<SDL_LogPriority>(priority_), "%s", format.data());
+			}
+			else
+			{
+				SDL_LogMessage(static_cast<SDL_LogCategory>(category_), static_cast<SDL_LogPriority>(priority_), format.data(), std::forward<Args>(args)...);
+			}
 		}
-	}
+	} // namespace log
 } // namespace de
 
-#define DE_LOG(priority, message, ...) de::log(de::log_category::Application, de::log_priority::priority, message, ##__VA_ARGS__)
-#define DE_LOGC(category, priority, message, ...) de::log(de::log_category::category, de::log_priority::priority, message, ##__VA_ARGS__)
+#define DE_LOG(priority_, format, ...) de::log::message(de::log::category::Application, de::log::priority::priority_, format, ##__VA_ARGS__)
+#define DE_LOGC(category_, priority_, format, ...) de::log::message(de::log::category::category_, de::log::priority::priority_, format, ##__VA_ARGS__)
