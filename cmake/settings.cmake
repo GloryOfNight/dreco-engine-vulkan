00d42eb47ab14b7c8229675d3f7a1aab01d9c8d1
Siarhei Dziki <siarhei.dziki@gmail.com>
2023-05-20 11:17:20 +0200

    - more cmake changes

---
+++ cmake/settings.cmake
@@ -0,0 +1,7 @@
+
+# binaries and libraries should be placed in the same directory
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin CACHE PATH "Runtime Output Directory")
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib CACHE PATH "Static Libraries Output Directory")
+
+# static libraries should be build with position independent code
+set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL "Use Position Independent Code")
