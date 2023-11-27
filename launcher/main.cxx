286915703407603190d94bf086d5a38aee350938
Siarhei Dziki <siarhei.dziki@gmail.com>
2023-11-27 17:20:45 -0500

    minor fixes

--- launcher/main.cxx
+++ launcher/main.cxx
@@ -25,6 +25,7 @@ int main()
 	if (const auto res = shader_compiler::attemptCompileShaders(DRECO_SHADERS_SOURCE_DIR, DRECO_SHADERS_BINARY_DIR); res != 0)
 	{
 		DE_LOG(Error, "Shader compilation failed with: %i", res);
+		return EXIT_FAILURE;
 	}
 
 	engine.run();
